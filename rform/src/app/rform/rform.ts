9947797e2c61999e5f0ea0169c3a282c50d797b2
Ahona-Bar <23051159@kiit.ac.in>
2025-07-01 16:01:03 +0530

    Files changed

--- rform/src/app/rform/rform.ts
+++ rform/src/app/rform/rform.ts
@@ -1,5 +1,6 @@
 
 import { NgFor, NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
